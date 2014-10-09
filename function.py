3e5130733c27627cf1a25c226779d408769e3c3b
NikhitaV <nikhitavaz@gmail.com>
2014-10-09 23:35:27 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,22 @@
+simplified-programming
+======================
+import os,sys
+def one():
+    print "This is the first version of code."
+    f_read=open('example2_2.txt','r').readlines()
+    f_write=open('example2_2.txt','w')
+    for eachline in f_read:
+        eachline=eachline.strip()+","+"null"
+        f_write.write(eachline+"\n")
+    f_write.close()
+    return
+
+def two():
+    print "CSV file is updated with default values."
+    return
+   
+def three():
+
+    print "This would be final version of code"
+    return
+    
